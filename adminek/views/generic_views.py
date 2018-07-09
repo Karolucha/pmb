32331e05d6f830d1f6c83d141d13d4679e7c5fb1
Karolinka <karolina.herlender@gmail.com>
2018-07-09 23:20:03 +0200

    Office hours ready to delete

--- adminek/views/generic_views.py
+++ adminek/views/generic_views.py
@@ -6,14 +6,22 @@ from django.views import View
 class BaseGenericView(View):
 
     def get(self, request, *args, **kwargs):
+        method = kwargs.get('method', 'get')
         object_name = kwargs['object_name']
         model_class = apps.get_model(app_label='app', model_name=object_name)
         context = model_class.get_context()
-        template_name = 'app/' + object_name.lower() + '_form.html'
         if kwargs['pk'] is not None:
             article = model_class.objects.get(id=kwargs['pk'])
             context['object'] = article
         print('context ', context)
+        if method == 'delete':
+            post_fix = '_delete.html'
+        elif method == 'list':
+            context['object_list'] = model_class.objects.all()
+            post_fix = '_list.html'
+        else:
+            post_fix = '_form.html'
+        template_name = 'app/' + object_name.lower() + post_fix
         return render(request, template_name, context)
 
     def post(self, request, *args, **kwargs):
@@ -24,15 +32,17 @@ class BaseGenericView(View):
             values = {field: request.POST[field] for field in fields
                       if field != 'id'}
             model_object = model_class(**values)
+            model_object.save()
         elif request.POST.get('delete'):
             model_object = model_class.objects.get(id=kwargs['pk'])
             model_object.delete()
+            model_object.save()
         else:
             model_object = model_class.objects.filter(id=kwargs['pk'])
             values = {field: request.POST[field] for field in fields
                       if field != 'id'}
             model_object.update(**values)
-        model_object.save()
+
         template_name = 'app/' + object_name.lower() + '_list.html'
         return render(request, template_name, {
             'object_list': model_class.objects.all()})
